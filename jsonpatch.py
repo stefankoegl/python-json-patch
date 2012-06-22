88bfb42102b510847f9cc1094db9b8f7c62408f6
Stefan Kögl <stefan@skoegl.net>
2012-06-22 21:18:09 +0200

    bump version to 0.2

--- jsonpatch.py
+++ jsonpatch.py
@@ -35,7 +35,7 @@ http://tools.ietf.org/html/draft-pbryan-json-patch-04"""
 
 # Will be parsed by setup.py to determine package metadata
 __author__ = 'Stefan Kögl <stefan@skoegl.net>'
-__version__ = '0.1'
+__version__ = '0.2'
 __website__ = 'https://github.com/stefankoegl/python-json-patch'
 __license__ = 'Modified BSD License'
 
