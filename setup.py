d65bd3ef3d7b1440713ec41ca568af5dd3807ba4
Stefan Kögl <stefan@skoegl.net>
2014-03-09 14:47:33 +0100

    add trove classifiers

--- setup.py
+++ setup.py
@@ -43,6 +43,27 @@ DESCRIPTION = docstrings[0]
 # Extract name and e-mail ("Firstname Lastname <mail@example.org>")
 AUTHOR, EMAIL = re.match(r'(.*) <(.*)>', AUTHOR_EMAIL).groups()
 
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Utilities',
+]
+
+
 setup(name=PACKAGE,
       version=VERSION,
       description=DESCRIPTION,
@@ -58,5 +79,6 @@ setup(name=PACKAGE,
             'jsondiff = jsondiff:main',
             'jsonpatch = jsonpatch:main',
         ]},
+      classifiers=CLASSIFIERS,
       **OPTIONS
 )
