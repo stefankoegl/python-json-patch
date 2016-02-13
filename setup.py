5cc9bee572f6207166122ac2ba9cecde0598930a
Stefan Kögl <stefan@skoegl.net>
2016-02-13 19:04:55 +0100

    Update trove classification to include Python 3.5

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Topic :: Software Development :: Libraries',
