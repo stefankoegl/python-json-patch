d1cfec3187bc5e8b9e43127848107d7f4bf3dd80
Stefan Kögl <stefan@skoegl.net>
2021-03-04 20:13:29 +0100

    Bump version to 1.31

--- jsonpatch.py
+++ jsonpatch.py
@@ -61,7 +61,7 @@ except ImportError:
 
 # Will be parsed by setup.py to determine package metadata
 __author__ = 'Stefan Kögl <stefan@skoegl.net>'
-__version__ = '1.30'
+__version__ = '1.31'
 __website__ = 'https://github.com/stefankoegl/python-json-patch'
 __license__ = 'Modified BSD License'
 
