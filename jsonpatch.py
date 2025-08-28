d64bd56802044cf5a2afa7bdab9402945c6544cf
jeonsworld <jeonsworld@gmail.com>
2025-08-28 17:52:16 +0900

    docs: update README with new append operation for chat streaming

--- jsonpatch.py
+++ jsonpatch.py
@@ -30,7 +30,7 @@
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-""" Apply JSON-Patches (RFC 6902) """
+""" Improved version of jsonpatch """
 
 from __future__ import unicode_literals
 
@@ -66,9 +66,9 @@ except ImportError:
     str = unicode
 
 # Will be parsed by setup.py to determine package metadata
-__author__ = 'Stefan Kögl <stefan@skoegl.net>'
-__version__ = '1.33'
-__website__ = 'https://github.com/stefankoegl/python-json-patch'
+__author__ = 'jeonsworld <jeonsworld@gmail.com>'
+__version__ = '1.33+chat0'
+__website__ = 'https://github.com/jeonsworld/python-json-patch-chat'
 __license__ = 'Modified BSD License'
 
 
