23d8ad98f7b6fa990adf10068f86c52abc0aa2d5
Angela Liss <59097311+angela-tarantula@users.noreply.github.com>
2025-10-11 17:05:55 -0400

    removed old test files (tests.py already updated)

--- issue2.py
+++
@@ -1,9 +0,0 @@
-from jsonpatch import apply_patch, make_patch
-
-old = ['a', 'b', ['d', 'e'], 'f']
-
-new = ['a', 'd', ['e', 'g']]
-
-patch = make_patch(old, new)
-
-assert apply_patch(old, patch) == new
