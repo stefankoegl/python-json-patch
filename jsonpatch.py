d02143e79dcea1fa0f01f98f602c009248f9476b
Angela Liss <59097311+angela-tarantula@users.noreply.github.com>
2025-10-12 11:32:51 -0400

    check for MutableSequence, not list
    This grants flexility for subclassing. Also removed a comment that
    previously existed because of discrepancy between _item_added and
    _item_removed that no longer exists.

--- jsonpatch.py
+++ jsonpatch.py
@@ -798,7 +798,7 @@ class DiffBuilder(object):
         if index is not None:
             op = index[2]
             parent_collection = op.pointer.to_last(self.dst_doc)[0]
-            if type(parent_collection) == list:
+            if isinstance(parent_collection, MutableSequence):
                 for v in self.iter_from(index):
                     op.key = v._on_undo_remove(op.path, op.key)
 
@@ -828,12 +828,8 @@ class DiffBuilder(object):
         new_index = self.insert(new_op)
         if index is not None:
             op = index[2]
-            # We can't rely on the op.key type since PatchOperation casts
-            # the .key property to int and this path wrongly ends up being taken
-            # for numeric string dict keys while the intention is to only handle lists.
-            # So we do an explicit check on the item affected by the op instead.
             parent_collection = op.pointer.to_last(self.dst_doc)[0]
-            if type(parent_collection) == list:
+            if isinstance(parent_collection, MutableSequence):
                 for v in self.iter_from(index):
                     op.key = v._on_undo_add(op.path, op.key)
 
